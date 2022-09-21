886cf5d1420d56c7d530bea308f670eb72cdcae9
LoweKumar <lowekr00@gmail.com>
2022-09-21 17:17:33 +0530

    split method in python

--- OperatorPrecedence/splitMethod.py
+++ OperatorPrecedence/splitMethod.py
@@ -6,4 +6,6 @@ the lazy dogs"""
 print(panagram.split())
 
 number="2,4,45,67,100,1"
-print(number.split(","))
+print(number.split(",")) # return list with comma seperated
+result = tuple(number.split(","))
+print(result) # returns tuple with comma seperated

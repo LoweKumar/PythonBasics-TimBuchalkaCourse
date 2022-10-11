0e9d48a4d2fc047f39668408d2e5e4620c1ec6ce
LoweKumar <lowekr00@gmail.com>
2022-10-11 13:39:55 +0530

    star_args demo

---
+++ OperatorPrecedence/star_args.py
@@ -0,0 +1,15 @@
+# def multiple_values(*args, file=None):
+#     for word in args[::-1]:
+#         print(word, end=' ', file=file)
+#
+# with open("star_args.txt", 'w') as fileObject:
+#    multiple_values("hello", "World", "Python", "java", "Flask", file = fileObject)
+
+def star_args(*args, file=None):
+    # to print in normal sentence we have to use for loop or else it will print in tuple form
+     for words in args:
+         print(words, end=' ', file=file)
+
+with open("multiplsArgsDemo.txt", 'w')as fileObject:
+    star_args("Hello", 'World', "Welcome", "To", "Python", 3.7, file=fileObject)
+

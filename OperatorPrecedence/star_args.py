b2b217e7acc887755d705d03847dc903ae90d961
LoweKumar <lowekr00@gmail.com>
2022-10-11 13:49:18 +0530

    multipls Args Demo Using KWargs

--- OperatorPrecedence/star_args.py
+++ OperatorPrecedence/star_args.py
@@ -5,11 +5,22 @@
 # with open("star_args.txt", 'w') as fileObject:
 #    multiple_values("hello", "World", "Python", "java", "Flask", file = fileObject)
 
-def star_args(*args, file=None):
-    # to print in normal sentence we have to use for loop or else it will print in tuple form
-     for words in args:
-         print(words, end=' ', file=file)
+# def star_args(*args, file=None):
+#     # to print in normal sentence we have to use for loop or else it will print in tuple form
+#      for words in args:
+#          print(words, end=' ', file=file)
+#
+# with open("multiplsArgsDemo.txt", 'w')as fileObject:
+#     star_args("Hello", 'World', "Welcome", "To", "Python", 3.7, file=fileObject)
 
-with open("multiplsArgsDemo.txt", 'w')as fileObject:
+
+# use of 'kwargs' - in the same above program
+def star_args(*args, **kwargs):
+    # to print in normal sentence we have to use for loop or else it will print in tuple form
+    for words in args:
+        print(words, end=' ', **kwargs)
+
+with open("multiplsArgsDemoUsingKWargs.txt", 'w')as fileObject:
     star_args("Hello", 'World', "Welcome", "To", "Python", 3.7, file=fileObject)
+
 
